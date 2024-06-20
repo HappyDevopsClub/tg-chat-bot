88d95474a07d6c7149f3d10b602f4e863f0021a2
Sergey Shevchenko <sergeyshevchdevelop@gmail.com>
2024-06-21 01:02:32 +0300

    feat: Update signals

--- main.go
+++ main.go
@@ -1,37 +1,38 @@
 package main
 
 import (
-	"context"
-	"os"
-	"os/signal"
+  "context"
+  "os"
+  "os/signal"
+  "syscall"
 
-	"github.com/go-telegram/bot"
-	"go.uber.org/zap"
+  "github.com/go-telegram/bot"
+  "go.uber.org/zap"
 
-	"github.com/HappyDevopsClub/tg-chat-bot/handlers"
+  "github.com/HappyDevopsClub/tg-chat-bot/handlers"
 )
 
 var log = zap.L()
 
 func main() {
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
+  ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+  defer cancel()
 
-	var opts []bot.Option
+  var opts []bot.Option
 
-	if os.Getenv("DEBUG") == "true" {
-		opts = append(opts, bot.WithDebug())
-	}
+  if os.Getenv("DEBUG") == "true" {
+    opts = append(opts, bot.WithDebug())
+  }
 
-	b, err := bot.New(
-		os.Getenv("TELEGRAM_BOT_TOKEN"),
-		opts...,
-	)
-	if err != nil {
-		log.Fatal("failed to create bot", zap.Error(err))
-	}
+  b, err := bot.New(
+    os.Getenv("TELEGRAM_BOT_TOKEN"),
+    opts...,
+  )
+  if err != nil {
+    log.Fatal("failed to create bot", zap.Error(err))
+  }
 
-	b.RegisterHandlerMatchFunc(handlers.WelcomeMessageFilter, handlers.WelcomeMessageHandler)
+  b.RegisterHandlerMatchFunc(handlers.WelcomeMessageFilter, handlers.WelcomeMessageHandler)
 
-	b.Start(ctx)
+  b.Start(ctx)
 }
